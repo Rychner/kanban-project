b0e7ffa4b839cd8c3df6cebf9ac5758c03f4e999
Rychner <milanrychner@gmail.com>
2025-05-24 15:02:16 +0700

    model and migration workspace

---
+++ app/Models/Workspace.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Enums\WorkspaceVisibility;
+
+class Workspace extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'slug',
+        'cover',
+        'logo',
+        'visibility',
+    ];
+
+    protected function castc()
+    {
+        return [
+            'visibility' => WorkspaceVisibility::class,
+        ];
+    }
+}
