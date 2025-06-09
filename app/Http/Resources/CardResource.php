cf9d6e87bc98f859bc17565311f7aa0a683d805c
Rychner <milanrychner@gmail.com>
2025-06-09 15:26:26 +0700

    workspace card redesign

--- app/Http/Resources/CardResource.php
+++ app/Http/Resources/CardResource.php
@@ -5,6 +5,8 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Carbon\Carbon;
+use App\Http\Resources\MemberResource;
+use App\Http\Resources\TaskResource;
 
 class CardResource extends JsonResource
 {
@@ -16,15 +18,26 @@ class CardResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'            => $this->id,
-            'user_id'       => $this->user_id,
-            'title'         => $this->title,
-            'workspace_id'  => $this->workspace_id,
-            'description'   => $this->description,
-            'status'        => $this->status,
-            'priority'      => $this->priority,
-            'created_at'    => $this->created_at->format('d M Y'),
-            'deadline'      => (int) Carbon::now()->diffInDays(Carbon::createFromFormat('Y-m-d', $this->deadline)),
+            'id'                => $this->id,
+            'user_id'           => $this->user_id,
+            'title'             => $this->title,
+            'workspace_id'      => $this->workspace_id,
+            'description'       => $this->description,
+            'status'            => $this->status,
+            'priority'          => $this->priority,
+            'created_at'        => $this->created_at->format('d M Y'),
+            'deadline'          => (int) Carbon::now()->diffInDays(Carbon::createFromFormat('Y-m-d', $this->deadline)),
+            'members'           => MemberResource::collection($this->members),
+            'member_count'      => $this->members_count,
+            'attachments'       => $this->attachments,
+            'attachments_count' => $this->attachments_count,
+            'has_attachment'    => $this->attachments()->exists(),
+            'tasks'             => TaskResource::collection($this->tasks),
+            'has_task'          => $this->tasks()->exists(),
+            'tasks_count'       => $this->tasks_count,
+            'percentage'        => $this->tasks_count > 0 
+                ? round(($this->tasks->where('is_completed', true)->count()/$this->tasks_count) * 100)
+                : 0,
         ];
     }
 }
--- resources/js/Pages/Workspace/Show.jsx
+++ resources/js/Pages/Workspace/Show.jsx
@@ -1,6 +1,6 @@
 import { ActionDialog } from '@/Components/ActionDialog';
 import GetPriorityBadge from '@/Components/GetPriorityBadge';
-import { Card, CardDescription, CardHeader, CardTitle } from '@/Components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/Components/ui/card';
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -11,7 +11,15 @@ import {
 import AppLayout from '@/Layouts/AppLayout';
 import { flashMessage } from '@/lib/utils';
 import { Link, router } from '@inertiajs/react';
-import { PiDotsThreeOutlineFill, PiPencilSimpleFill, PiPlus, PiTrashSimpleFill } from 'react-icons/pi';
+import {
+    PiCheckSquare,
+    PiDotsThreeOutlineFill,
+    PiLinkSimple,
+    PiPencilSimpleFill,
+    PiPlus,
+    PiTrashSimpleFill,
+    PiUser,
+} from 'react-icons/pi';
 import { toast } from 'sonner';
 
 export default function Show({ ...props }) {
@@ -165,6 +173,58 @@ export default function Show({ ...props }) {
                                                     {card.description}
                                                 </CardDescription>
                                             </CardHeader>
+                                            <CardContent>
+                                                <div className="flex flex-col space-y-8">
+                                                    {card.has_task && (
+                                                        <div>
+                                                            <div className="mb-1.5 flex items-center justify-between">
+                                                                <p className="text-sm leading-relaxed tracking-tighter text-muted-foreground">
+                                                                    <span className="font-medium text-red-500">
+                                                                        {card.percentage} of 100
+                                                                    </span>
+                                                                </p>
+                                                                <p className="text-sm leading-relaxed tracking-tighter text-muted-foreground">
+                                                                    {card.deadline > 0 ? (
+                                                                        <span>{card.deadline} days left</span>
+                                                                    ) : card.deadline == 0 ? (
+                                                                        <span className="text-yellow-500">
+                                                                            Today is the Deadline
+                                                                        </span>
+                                                                    ) : (
+                                                                        <span className="text-red-500">Overdue</span>
+                                                                    )}
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    )}
+                                                    <div className="flex items-center justify-between gap-x-4">
+                                                        {card.has_task && (
+                                                            <div className="flex items-center gap-x-1">
+                                                                <PiCheckSquare className="h-4 w-4 text-muted-foreground" />
+                                                                <span className="text-sm leading-relaxed tracking-tighter text-muted-foreground">
+                                                                    {card.tasks_count} Tasks
+                                                                </span>
+                                                            </div>
+                                                        )}
+                                                        {card.member_count > 1 && (
+                                                            <div className="flex items-center gap-x-1">
+                                                                <PiUser className="h-4 w-4 text-muted-foreground" />
+                                                                <span className="text-sm leading-relaxed tracking-tighter text-muted-foreground">
+                                                                    {card.member_count} Members
+                                                                </span>
+                                                            </div>
+                                                        )}
+                                                        {card.has_attachment && (
+                                                            <div className="flex items-center gap-x-1">
+                                                                <PiLinkSimple className="h-4 w-4 text-muted-foreground" />
+                                                                <span className="text-sm leading-relaxed tracking-tighter text-muted-foreground">
+                                                                    {card.attachments_count} Files
+                                                                </span>
+                                                            </div>
+                                                        )}
+                                                    </div>
+                                                </div>
+                                            </CardContent>
                                         </Card>
                                     ))}
                             </div>
